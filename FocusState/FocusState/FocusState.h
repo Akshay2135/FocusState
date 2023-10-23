5c5e79fa38d305e3d2884f6273ed282251273142
Akshay Chavan <a.diliprao.chavan@accenture.com>
2023-10-23 11:02:15 +0530

    Corrected the Framework

---
+++ FocusState/FocusState/FocusState.h
@@ -0,0 +1,18 @@
+//
+//  FocusState.h
+//  FocusState
+//
+//  Created by Akshay Diliprao Chavan on 23/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FocusState.
+FOUNDATION_EXPORT double FocusStateVersionNumber;
+
+//! Project version string for FocusState.
+FOUNDATION_EXPORT const unsigned char FocusStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FocusState/PublicHeader.h>
+
+
